b2be8e914a665bba85a49134e88906103d99020a
Charles Darwin D.C. Pobre <cpobre.peg@gmail.com>
2017-05-12 22:32:34 +0800

    added device model and spec

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,5 @@
 class Device < ApplicationRecord
+  validates :device_mac, presence: true
+  validates :serial_no, presence: true
+  validates :name, presence: true
 end
--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Device, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:device_mac) }
+  it { is_expected.to validate_presence_of(:serial_no) }
+  it { is_expected.to validate_presence_of(:name) }
 end
